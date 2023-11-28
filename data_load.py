90e6d361057edf8ef2a90c7bdb3e8944b8814628
noproblemdude <leonardrolandvictor@googlemail.com>
2023-11-28 15:52:46 +0100

    data_load changed for final delivery

--- data_load.py
+++ data_load.py
@@ -1,20 +1,3 @@
-# Lint as: python3
-# Copyright 2019 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-# pylint: disable=g-bad-import-order
-
 """Load data from the specified paths and format them for training."""
 
 from __future__ import absolute_import
@@ -25,8 +8,6 @@ import json
 import numpy as np
 import tensorflow as tf
 
-from data_augmentation import augment_data
-
 LABEL_NAME = "SEMS"
 DATA_NAME = "rows"
 
@@ -36,10 +17,9 @@ class DataLoader(object):
 
   def __init__(self, train_data_path, valid_data_path, test_data_path,
                seq_length):
-    #CHANGE self.dim = 15
     self.dim = 10
     self.seq_length = seq_length
-    self.label2id = {"0": 0, "1": 1, "2": 2, "3": 3,  "4": 4,  "5": 5,  "6": 6,  "7": 7,  "8": 8}#,  "9": 9, "10": 10, "11": 11, "12": 12}
+    self.label2id = {"0": 0, "1": 1, "2": 2, "3": 3,  "4": 4,  "5": 5,  "6": 6,  "7": 7,  "8": 8}
     self.train_data, self.train_label, self.train_len = self.get_data_file(
         train_data_path, "train")
     self.valid_data, self.valid_label, self.valid_len = self.get_data_file(
@@ -53,60 +33,49 @@ class DataLoader(object):
     label = []
     with open(data_path, "r") as f:
       lines = f.readlines()
-      for idx, line in enumerate(lines):  # pylint: disable=unused-variable
+      for idx, line in enumerate(lines):
+        # for each line from the JSON, which represents a Dataset in Dict form, convert it back into python objects 
         dic = json.loads(line)
         data.append(dic[DATA_NAME])
         label.append(dic[LABEL_NAME])
-    #if data_type == "train":
-    #  data, label = augment_data(data, label)
     length = len(label)
     print(data_type + "_data_length:" + str(length))
     return data, label, length
 
-  def pad(self, data, seq_length, dim):
-    """Get neighboor padding."""
-    #noise_level = 20
-    noise_level = 0
-    padded_data = []
-    # Before- Neighbour padding
-    ##tmp_data = (np.random.rand(seq_length, dim) - 0.5) * noise_level + data[0]
-    tmp_data = []#  data[0]
+  def sliceData(self, data, seq_length):
+    """Get slice of Data"""
+    slicedData = []
+    tmp_data = []
     tmp_data[(seq_length -
               min(len(data), seq_length)):] = data[:min(len(data), seq_length)]
-    #padded_data.append(tmp_data)
-    # After- Neighbour padding
-    ##tmp_data = (np.random.rand(seq_length, dim) - 0.5) * noise_level + data[-1]
-    #tmp_data = data[-1]
     tmp_data[:min(len(data), seq_length)] = data[:min(len(data), seq_length)]
-    padded_data.append(tmp_data)
-    return padded_data
-    #return data
+    slicedData.append(tmp_data)
+    return slicedData
 
-  def format_support_func(self, padded_num, length, data, label):
+  def format_support_func(self, slice_num, length, data, label):
     """Support function for format.(Helps format train, valid and test.)"""
-    # Add 2 padding, initialize data and label
-    length *= padded_num
+    # Add 1 padding, initialize data and label
+    length *= slice_num
     features = np.zeros((length, self.seq_length, self.dim))
     labels = np.zeros(length)
-    # Get padding for train, valid and test
+    # Get slices for train, valid and test
     for idx, (data, label) in enumerate(zip(data, label)):
-      padded_data = self.pad(data, self.seq_length, self.dim)
-      for num in range(padded_num):
-        features[padded_num * idx + num] = padded_data[num]
-        labels[padded_num * idx + num] = self.label2id[label]
+      sliced_data = self.sliceData(data, self.seq_length)
+      for num in range(slice_num):
+        features[slice_num * idx + num] = sliced_data[num]
+        labels[slice_num * idx + num] = self.label2id[label]
     # Turn into tf.data.Dataset
     dataset = tf.data.Dataset.from_tensor_slices(
         (features, labels.astype("int32")))
     print(dataset)
     return length, dataset
-
+  
   def format(self):
     """Format data(including padding, etc.) and get the dataset for the model."""
-    #CHANGED padded_num = 2
-    padded_num = 1
+    slice_num = 1
     self.train_len, self.train_data = self.format_support_func(
-        padded_num, self.train_len, self.train_data, self.train_label)
+        slice_num, self.train_len, self.train_data, self.train_label)
     self.valid_len, self.valid_data = self.format_support_func(
-        padded_num, self.valid_len, self.valid_data, self.valid_label)
+        slice_num, self.valid_len, self.valid_data, self.valid_label)
     self.test_len, self.test_data = self.format_support_func(
-        padded_num, self.test_len, self.test_data, self.test_label)
+        slice_num, self.test_len, self.test_data, self.test_label)
